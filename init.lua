701af11ba57733b5bfb7efc6802489fb541b93f9
frederikja <fjaplv64@gmail.com>
2025-12-06 19:50:16 +0100

    more nvim configs

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 -- Options
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.number = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 5
+vim.opt.cursorline = true
+vim.opt.inccommand = 'split'
 vim.opt.relativenumber = true
 vim.opt.wrap = false
 vim.opt.tabstop = 2
@@ -10,34 +17,88 @@ vim.opt.signcolumn = "yes"
 vim.g.mapleader = " "
 
 -- Keymaps
-vim.keymap.set('n', '<leader>c', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>wj', ':wincmd j<CR>')
-vim.keymap.set('n', '<leader>wk', ':wincmd k<CR>')
-vim.keymap.set('n', '<leader>wh', ':wincmd h<CR>')
-vim.keymap.set('n', '<leader>wl', ':wincmd l<CR>')
-vim.keymap.set('n', '<leader>ws', ':wincmd s<CR>')
-vim.keymap.set('n', '<leader>wv', ':wincmd v<CR>')
+vim.keymap.set('n', '<leader>c', '<cmd>update<CR> <cmd>source<CR>')
+vim.keymap.set('n', '<leader>q', '<cmd>quit<CR>')
+vim.keymap.set('n', '<leader>w', '<cmd>write<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('i', '<C-j>', '<C-o>j')
+vim.keymap.set('i', '<C-k>', '<C-o>k')
+vim.keymap.set('i', '<C-h>', '<C-o>h')
+vim.keymap.set('i', '<C-l>', '<C-o>l')
+-- move window
+vim.keymap.set('n', '<C-j>', '<cmd>wincmd j<CR>')
+vim.keymap.set('n', '<C-k>', '<cmd>wincmd k<CR>')
+vim.keymap.set('n', '<C-h>', '<cmd>wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', '<cmd>wincmd l<CR>')
+-- new window
+vim.keymap.set('n', '<leader>ws', '<cmd>wincmd s<CR>')
+vim.keymap.set('n', '<leader>wv', '<cmd>wincmd v<CR>')
+
+-- File browser
+vim.pack.add({ { src = "https://github.com/nvim-mini/mini.pick" } })
+require('mini.pick').setup()
+vim.keymap.set('n', '<leader>ff', '<cmd>Pick files<CR>')
+vim.keymap.set('n', '<leader>fb', '<cmd>Pick buffers<CR>')
+vim.keymap.set('n', '<leader>fh', '<cmd>Pick help<CR>')
+
+-- LSP
+vim.pack.add({ { src = "https://github.com/neovim/nvim-lspconfig" } })
+vim.lsp.enable({ "lua_ls" })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
-vim.keymap.set('n', '<leader>ff', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>fb', ':Pick buffers<CR>')
-vim.keymap.set('n', '<leader>fh', ':Pick help<CR>')
 
--- Plugins
-vim.pack.add({
-	{ src = "https://github.com/catppuccin/nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/nvim-mini/mini.pick" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-})
-require('mini.pick').setup()
+-- Theme
+vim.pack.add({ { src = "https://github.com/catppuccin/nvim" } })
+vim.cmd.colorscheme "catppuccin-mocha"
+
+vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter" } })
 require('nvim-treesitter.configs').setup({
 	auto_install = true
 })
 
--- Theme
-vim.cmd.colorscheme "catppuccin-mocha"
-vim.lsp.enable({ "lua_ls" })
+-- Auto complete
+vim.pack.add({
+	{ src = "https://github.com/hrsh7th/nvim-cmp" },
+	{ src = "https://github.com/hrsh7th/vim-vsnip" },
+	{ src = "https://github.com/hrsh7th/cmp-vsnip" },
+	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+	{ src = "https://github.com/hrsh7th/cmp-path" },
+	{ src = "https://github.com/onsails/lspkind.nvim" },
+})
+local cmp = require("cmp")
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	mapping = {
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+	},
+	formatting = {
+		format = function(_, vim_item)
+			vim.cmd("packadd lspkind-nvim")
+			vim_item.kind = require("lspkind").presets.codicons[vim_item.kind]
+					.. "  "
+					.. vim_item.kind
+			return vim_item
+		end,
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
+		{ name = "path" },
+	},
+})
